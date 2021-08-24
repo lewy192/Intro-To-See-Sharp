26c368b8018e6367dad4c05b23008dca7859732a
lewy192 <lewis.james.hill@outlook.com>
2021-08-24 12:46:00 +1000

    feat: cli print name and date

--- Hello-World/Program.cs
+++ Hello-World/Program.cs
@@ -6,7 +6,12 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.WriteLine(Console.ReadKey(true));
         }
     }
 }
